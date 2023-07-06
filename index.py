5cf36fbcd12b97b7f90a579c035a1e8860428819
w2038 <w4gn3r2038@gmail.com>
2023-07-06 19:15:53 -0300

    pagina com flask

---
+++ index.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+lista_usuarios = ['wagner', 'lorena', 'max', 'vinicius']
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html", lista_usuarios=lista_usuarios)
+
+if __name__ == "__main__":
+    app.run(debug=True)
